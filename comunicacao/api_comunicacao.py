e32a45e4fe668398b833e45eb33db16d391ee02c
Rafael Araujo Raimundo <rafael.araujo@combio.com.br>
2024-11-22 11:56:41 -0300

    alterando o email

--- comunicacao/api_comunicacao.py
+++ comunicacao/api_comunicacao.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from administration.models import Parametro
 from .schema import PapelDeParedeSchema
 import logging
+from suporte.tasks import verificar_bloqueios_pendentes
 
 routerParametro = Router()
 
@@ -21,7 +22,7 @@ def get_papeldeparede(request):
     url_papeldeparede = get_or_default('url_papeldeparede','http://179.191.91.6:810/media/papeldeparede/papelparede.jpg')
     url_bloqueio = get_or_default('url_bloqueio', 'http://179.191.91.6:810/media/papeldeparede/bloqueio.jpg')
     pasta_papeldeparede = get_or_default('pasta_papelparede','C:/combio/wallpaper-combio/')
-
+    
     return PapelDeParedeSchema(
         url_papeldeparede=url_papeldeparede,
         url_bloqueio=url_bloqueio,
