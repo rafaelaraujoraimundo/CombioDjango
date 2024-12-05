5c273b36d5d909ed6f675860381f6abf0267eb57
Rafael Araujo Raimundo <rafael.araujo@combio.com.br>
2024-12-05 14:38:34 -0300

    Inclusão da pagina principal

--- menu/views.py
+++ menu/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from inventario.models import Celular, Computador, ControleFones, Controlekit, Monitor
+from cofre.models import PasswordManager
 # Create your views here.
 
 @login_required(login_url='account_login')
 def index(request):
-    title = 'Sistema Combio'
+
+    totals = {
+        "Teclado/Mouse": Controlekit.objects.count(),
+        "Fones": ControleFones.objects.count(),
+        "Monitores": Monitor.objects.count(),
+        "Computadores": Computador.objects.count(),
+        "Celulares": Celular.objects.count(),
+        "Passwords": PasswordManager.objects.count(),
+    }
+
+    colors = [
+        "--iq-color1", "--iq-color2", "--iq-color3",
+        "--iq-color4", "--iq-color5", "--iq-color6",
+    ]
+
+    
+    totals_with_colors = [
+        {"model": model, "count": count, "color": color}
+        for (model, count), color in zip(totals.items(), colors)
+    ]
+
+
+    title = 'Sistemas Combio'
     context = {
         "title": title, 
+       "totals_with_colors": totals_with_colors,
     }
 
     return render(request, "menu/principal.html", context)
